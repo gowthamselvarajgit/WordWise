5868577d64de328c552d7d9a9c2fdde84476e393
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-07-25 18:35:07 +0530

    Updated for Deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080/api/auth",
+  baseURL: "http://65.0.106.151:8080/api/auth",
   headers: {
     "Content-Type": "application/json", 
   },
